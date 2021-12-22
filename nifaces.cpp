1cfb64934b9555b5dc0ae0aba6e6181ba4c8f3bd
Ahmed Mohammed <abusaadma7@gmail.com>
2021-12-22 23:58:18 +0300

    fix a mistake I made in the nifaces files. Shouldnt have hurried so much when making commits

--- nifaces.cpp
+++ nifaces.cpp
@@ -4,29 +4,28 @@
 #pragma comment(lib,"iphlpapi.lib")
 #pragma comment(lib,"ws2_32.lib")
 
-PMIB_IPADDRTABLE addrs;
 
 extern "C" __declspec(dllexport) char* getaddresses(void) {
-	ULONG size = 0;
-	GetIpAddrTable(addrs,&size,FALSE); //calculate required size
-	addrs = (PMIB_IPADDRTABLE)malloc(size);
-	GetIpAddrTable(addrs, &size, FALSE);
+	ULONG size = sizeof(MIB_IPADDRTABLE);
+	PMIB_IPADDRTABLE addrs = (PMIB_IPADDRTABLE)malloc(size);
 
-	char* retlist = (char*)malloc(15ull * addrs->dwNumEntries);
+	if (GetIpAddrTable(addrs, &size, FALSE) == ERROR_INSUFFICIENT_BUFFER) {//this function sets the size if it wasnt enough
+		free(addrs);
+		addrs = (PMIB_IPADDRTABLE)malloc(size);
+		GetIpAddrTable(addrs, &size, FALSE);
+	}
+
+	char* retlist = (char*)malloc(15ull * addrs->dwNumEntries); //an ip address is 15 chars maximum
 
 	unsigned c = 0u;
 	unsigned i;
 	for (i = 0; i < (addrs->dwNumEntries) - 1; i++) {
 		DWORD addr = addrs->table[i].dwAddr;
-		DWORD mask = addrs->table[i].dwMask;
-		DWORD broad = addr | (~mask);
-		c += sprintf(retlist+c,"%d.%d.%d.%d ", broad & 0xffu, (broad >> 8) & 0xffu, (broad >> 16) & 0xffu, broad >> 24);
+		c += sprintf(retlist+c,"%d.%d.%d.%d ", addr & 0xffu, (addr >> 8) & 0xffu, (addr >> 16) & 0xffu, addr >> 24);
 	}
 	
 	DWORD addr = addrs->table[i].dwAddr;
-	DWORD mask = addrs->table[i].dwMask;
-	DWORD broad = addr | (~mask);
-	sprintf(retlist + c, "%d.%d.%d.%d", broad & 0xffu, (broad >> 8) & 0xffu, (broad >> 16) & 0xffu, broad >> 24);
+	sprintf(retlist + c, "%d.%d.%d.%d", addr & 0xffu, (addr >> 8) & 0xffu, (addr >> 16) & 0xffu, addr >> 24);
 	
 	free(addrs);	
 	return retlist;
