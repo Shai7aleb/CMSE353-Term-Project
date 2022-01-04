207c281c7849fafb4de0673a37845f983d21d8fb
OsamahAli-1 <96534700+OsamahAli-1@users.noreply.github.com>
2022-01-04 22:11:18 +0200

    Add files via upload

---
+++ Market/src/contracts/Market.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract Market{
+    string public name;
+
+    constructor() public{
+        name ="CMSE353 Crypto";
+    }
+}
---
+++ Market/test/Market.test.js
@@ -0,0 +1,24 @@
+const Market = artifacts.require("./Market.sol");
+
+contract("Market", (accounts) => {
+  let market;
+
+  before(async () => {
+    market = await Market.deployed();
+  });
+
+  describe("deployment", async () => {
+    it("deploys successfully", async () => {
+      const address = await market.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+
+    it("has a name", async () => {
+      const name = await market.name();
+      assert.equal(name, "CMSE353 Crypto");
+    });
+  });
+});
