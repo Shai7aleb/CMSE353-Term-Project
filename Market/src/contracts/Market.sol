7dfd99ecff967af7e79ead9f223ab8ad4ff0027b
salsabeel-tn <salsabeelj66@gmail.com>
2022-01-05 23:22:21 +0300

    PART 2 done
    smart contract for selling a product completed

--- Market/src/contracts/Market.sol
+++ Market/src/contracts/Market.sol
@@ -2,8 +2,41 @@ pragma solidity ^0.5.0;
 
 contract Market{
     string public name;
+    uint public productCount = 0;
+    mapping(uint => Product ) public products;
+    struct Product{
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
 
     constructor() public{
         name ="CMSE353 Crypto";
     }
+
+    function createProduct(string memory _name, uint _price) public {
+        //params are correct
+        //require a name
+        require(bytes(_name).length > 0);
+        //require a valid price
+        require(_price > 0);
+        //increment product
+        productCount ++;
+        //create the product
+        products[productCount] = Product(productCount, _name,_price,msg.sender,false);
+        //trigger an event
+        emit ProductCreated(productCount, _name,_price,msg.sender,false);
+
+    }
+    // PART 3 
 }
--- Market/test/Market.test.js
+++ Market/test/Market.test.js
@@ -1,6 +1,8 @@
 const Market = artifacts.require("./Market.sol");
-
-contract("Market", (accounts) => {
+require('chai')
+   .use(require('chai-as-promised'))
+   .should()
+contract("Market", ([deployer, seller, buyer]) => {
   let market;
 
   before(async () => {
@@ -21,4 +23,31 @@ contract("Market", (accounts) => {
       assert.equal(name, "CMSE353 Crypto");
     });
   });
+
+  describe("products", async () => {
+    let result, productCount
+    before(async () => {
+      result = await market.createProduct('iphone 7',web3.utils.toWei('1','Ether'), {from : seller});
+      productCount = await market.productCount();
+    });
+
+    it("creates products", async () => {
+      //success
+      assert.equal(productCount, 1);
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(),productCount.toNumber(), 'id is correct')
+      assert.equal(event.name, 'iphone 7', 'name is correct')
+      assert.equal(event.price, '1000000000000000000','price is correct')
+      assert.equal(event.owner, seller, 'name is correct')
+      assert.equal(event.purchased, false, 'purchased is correct')
+      //failure
+      //product should have a name
+      await await market.createProduct('',web3.utils.toWei('1','Ether'), {from : seller}).should.be.rejected;
+      //product should have a price > 0 Ether
+      await await market.createProduct('iphone 10',0, {from : seller}).should.be.rejected;
+    });
+    //  PART 3 
+  });
+
+
 });
